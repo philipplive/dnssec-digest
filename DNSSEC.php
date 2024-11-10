8536bffeec271de1ed2615888ccbb545287f19da
Phil <info@philipp.live>
2024-11-10 22:46:25 +0100

    Publish

---
+++ DNSSEC.php
@@ -0,0 +1,20 @@
+<?php
+
+class DNSSEC {
+	/**
+	 * @param string $publicKeyBase64 Schlüssel (Base64 encoded)
+	 * @param string $domain (IDNA)
+	 * @param int $hashType
+	 * @param int $algorithmType
+	 * @param int $keyType
+	 * @return string
+	 */
+	public static function getDigestFromPublicKey(string $publicKeyBase64, string $domain, int $hashType = 3, int $algorithmType = 13, int $keyType = 257): string {
+		$domainHeader = '';
+
+		foreach (explode('.', $domain) as $part)
+			$domainHeader .= pack('C', strlen($part)).$part;
+
+		return openssl_digest($domainHeader.pack('C', 0).pack('n', $keyType).pack('C', $hashType).pack('C', $algorithmType).base64_decode($publicKeyBase64), 'sha256');
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Erzeuge aus einem Public-Key einen Digest (DNSSEC)
+
+Der Digest besteht aus einer Kombination aus dem Public-Key, dem Domainnamen, dem Key-Typ, dem Algorithmus-Typ und dem Hash-Typ.
+
+## PHP
+Die folgende Funktion erzeugt den entsprechenden Digest mittels **\DNSSEC::getDigestFromPublicKey(...)**.
+
+## Unix-Systeme
+Der Digest kann mittels **dnssec-dsfromkey** erzeugt werden.
