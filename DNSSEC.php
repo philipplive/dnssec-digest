e7fcde4493a62be349c4d7937faec0d3c5460133
Phil <info@philipp.live>
2024-11-11 20:58:08 +0100

    Publish

--- DNSSEC.php
+++ DNSSEC.php
@@ -9,12 +9,12 @@ class DNSSEC {
 	 * @param int $keyType
 	 * @return string
 	 */
-	public static function getDigestFromPublicKey(string $publicKeyBase64, string $domain, int $hashType = 3, int $algorithmType = 13, int $keyType = 257): string {
+	public static function getDigestFromPublicKey(string $publicKeyBase64, string $domain, int $hashType = 3, int $algorithmType = 13, int $keyType = 257, string $digestAlgorithm = 'sha256'): string {
 		$domainHeader = '';
 
 		foreach (explode('.', $domain) as $part)
 			$domainHeader .= pack('C', strlen($part)).$part;
 
-		return openssl_digest($domainHeader.pack('C', 0).pack('n', $keyType).pack('C', $hashType).pack('C', $algorithmType).base64_decode($publicKeyBase64), 'sha256');
+		return openssl_digest($domainHeader.pack('C', 0).pack('n', $keyType).pack('C', $hashType).pack('C', $algorithmType).base64_decode($publicKeyBase64), $digestAlgorithm);
 	}
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Erzeuge aus einem Public-Key einen Digest (DNSSEC)
 
-Der Digest welcher für die DNSSEC DS-Records verwendet wird, stellt den Hash-Wert (SHA256) aus der Kombination des Public-Keys, dem Domainnamen, dem Key-Typ, dem Algorithmus-Typ und dem Hash-Typ dar.
+Der Digest welcher für die DNSSEC DS-Records verwendet wird, stellt den Hash-Wert aus der Kombination des Public-Keys, dem Domainnamen, dem Key-Typ, dem Algorithmus-Typ und dem Hash-Typ dar. 
+Mehr dazu unter https://www.rfc-editor.org/rfc/rfc4034 und https://de.wikipedia.org/wiki/DS_Resource_Record
 
 ## PHP
 Die folgende Funktion erzeugt den entsprechenden Digest mittels **\DNSSEC::getDigestFromPublicKey(...)**.
